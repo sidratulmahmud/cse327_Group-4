e8ed2d7c7af0ffe13223c4c0d83a484529284549
sidratulmahmud <63224014+sidratulmahmud@users.noreply.github.com>
2021-09-05 19:30:47 +0600

    Delete lib directory

--- lib/model/products.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class Product{
-  final String name;
-  final String image;
-  final int price;
-  final String detaials;
-  Product({@required this.name,@required this.image,@required this.price,@required this.detaials}
-  );
-}
